8279c175a17e4e62c0993ce16ace8f571f34c62e
maicol <castillomaicol214444@gmail.com>
2026-06-13 19:34:07 -0500

    Actividad semana 3

---
+++ programacion_tradicional/# Programa tradicional para registrar un.py
@@ -0,0 +1,20 @@
+# Programa tradicional para registrar una mascota
+
+def registrar_mascota():
+    nombre = input("Ingrese el nombre de la mascota: ")
+    especie = input("Ingrese la especie de la mascota: ")
+    edad = input("Ingrese la edad de la mascota: ")
+
+    return nombre, especie, edad
+
+
+def mostrar_mascota(nombre, especie, edad):
+    print("\n--- INFORMACIÓN DE LA MASCOTA ---")
+    print("Nombre:", nombre)
+    print("Especie:", especie)
+    print("Edad:", edad, "años")
+
+
+# Programa principal
+nombre, especie, edad = registrar_mascota()
+mostrar_mascota(nombre, especie, edad)
